3f21f8bbe9a1b874bbdecce733beb26d29cd7220
taylor gardin <vp9570vg@go.minnstate.edu>
2022-01-20 13:29:43 -0600

    finished assignment 2

--- assignment.R
+++ assignment.R
@@ -1,4 +1,19 @@
+library(palmerpenguins)
 penguins
 ggplot(data = penguins) + geom_point(mapping = aes(x=body_mass_g, y=flipper_length_mm))
 ggplot(data = penguins) + geom_point(mapping = aes(x=body_mass_g, y=flipper_length_mm, color = species)
                                      
+ggplot(data = penguins) + geom_point(mapping = aes(x = body_mass_g, y =
+                                                     flipper_length_mm))
+ggplot(data = penguins) + geom_point(mapping = aes(
+  x = body_mass_g,
+  y = flipper_length_mm,
+  color = species,
+  shape = island
+))
+ggplot(data = penguins) + geom_point(mapping = aes(
+  x = body_mass_g,
+  y = flipper_length_mm,
+  color = species,
+  shape = island
+)) + facet_grid( species ~ island)
